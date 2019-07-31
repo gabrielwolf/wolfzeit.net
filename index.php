a0cae589a632f6e272902aafba46e5aa4acdcccc
Gabriel Wolf <gabriel.wolf@wolfzeit.com>
2019-07-31 18:13:02 +0200

    Clean and beautiful. Removed texts.

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
     <title>W O L F Z E I T.net</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <?php if(strstr($_SERVER['HTTP_USER_AGENT'], 'Safari') && !strstr($_SERVER['HTTP_USER_AGENT'], 'Chrome')) { ?>
-        <link rel="stylesheet" href="https://wolfzeit.net/home/styles/safari.css">
+        <link rel="stylesheet" href="https://wolfzeit.net/styles/safari.css">
     <?php } else { ?>
-        <link rel="stylesheet" href="https://wolfzeit.net/home/styles/main.css">
+        <link rel="stylesheet" href="https://wolfzeit.net/styles/main.css">
     <?php } ?>
 </head>
 
@@ -24,18 +24,10 @@
     <header>
         <h1>W O L F Z E I T.net</h1>
         <?php if(strstr($_SERVER['HTTP_USER_AGENT'], 'Safari') && !strstr($_SERVER['HTTP_USER_AGENT'], 'Chrome')) { ?>
-            <img src="https://wolfzeit.net/home/images/logo-wolfzeit-net.png" title="W O L F Z E I T.net" />
+            <img src="https://wolfzeit.net/images/logo-wolfzeit-net.png" title="W O L F Z E I T.net" />
         <?php } else { ?>
-            <img src="https://wolfzeit.net/home/images/logo-wolfzeit-net.svg" onerror="this.onerror=null; this.src='https://wolfzeit.net/home/images/logo-wolfzeit-net.png'" title="W O L F Z E I T.net" />
+            <img src="https://wolfzeit.net/images/logo-wolfzeit-net.svg" onerror="this.onerror=null; this.src='https://wolfzeit.net/images/logo-wolfzeit-net.png'" title="W O L F Z E I T.net" />
         <?php } ?>
-<!--
-        <ul>
-            <li><a href="http://www.der-herr-pianist.de" title="Meine Leidenschaft">der-herr-pianist.de</a></li>
-            <li><a href="https://www.blauestunde.photo" title="Meine Leidenschaft">blauestunde.photo</a></li>
-            <li><a href="https://www.wolfzeit.com" title="Meine Selbständigkeit">wolfzeit.com</a></li>
-        </ul>
--->
-        <p>Server down for maintenance.</p>
     </header>
 </body>
 
